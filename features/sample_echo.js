0780f9b1f446a13a0ed27770f6d95cc6efc6f227
Oleksandr Terletskyy <oles.terletskiy@gmail.com>
2019-10-07 13:45:45 +0300

    Add bot

---
+++ features/sample_echo.js
@@ -0,0 +1,281 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+const request = require("request");
+const fetch = require('node-fetch');	
+
+function GetRepoURL(user)
+{
+	var gitlabRepos = {};
+	gitlabRepos['U4KSJ2RJ8'] = "14042156"; //terko
+	gitlabRepos['U58MWTQ1J'] = "14042181"; //@oz
+	gitlabRepos['U585UFVM3'] = "14042186"; //@naz0r
+	gitlabRepos['U7N52PR6X'] = "14042138"; //@Oleksandr Orchakov 
+	gitlabRepos['U7LJKNF96'] = "14042203"; //@Max Karpinsky 
+	gitlabRepos['UC94FSJ4V'] = "14042172"; //@purplebread 
+	gitlabRepos['U59UF4SF8'] = "14042193"; //@bromm
+	gitlabRepos['UC1N5KQ4F'] = null; //@romko
+	gitlabRepos['U6BNEMWKD'] = "14042762"; //@Maryan Tytyunnuk
+	gitlabRepos['U9Z1M4TKK'] = null; //@Alexandr Doroshenko
+	
+	return 'https://gitlab.com/api/v4/projects/' + gitlabRepos[user] + '/repository/branches?private_token=jdtk93Az_2x3XupkrkH_';
+}
+
+function LogBranches(branches)
+{
+	for(var i = 0; i < branches.length; i++)
+	  	{
+	  		console.log(branches[i].name);
+	  	}
+}
+
+function GetBranchesBlocks(branches, user)
+{
+	var blocks = 
+	{
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Please, select a *branch:*"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "static_select",
+					"placeholder": {
+						"type": "plain_text",
+						"text": "Select an item",
+						"emoji": true
+					},
+					"options": [
+					]
+				}
+			]
+		}
+	]
+}
+
+	for(var i = 0; i < branches.length; i++)
+  	{
+  		blocks.blocks[1].elements[0].options.push(addBranchOption(branches[i].name, user));
+  	}
+
+  	return blocks;
+}
+
+function addBranchOption(branch, user)
+{
+	return {
+								"text": {
+									"type": "plain_text",
+									"text": branch,
+									"emoji": true
+								},
+								"value": JSON.stringify({"branch" : branch, "user" : user})
+							};
+}
+
+async function onUserSelected(bot, message)
+{
+	var user = message.actions[0].selected_user;
+	url = GetRepoURL(user);
+
+	var error = null;
+
+	if (url != null)
+	{
+		const branchesResponse = await fetch(url, {});
+		const branches = await branchesResponse.json();
+
+		if (branches.message)
+		{
+			error = branches.message;
+		}
+		console.log(await bot.replyInteractive(message, GetBranchesBlocks(branches, user)));
+		// LogBranches(branches);
+	}
+	else
+	{
+		error = "user doesn't have any games";
+	}
+
+	if (error)
+	{
+		await bot.replyInteractive(message, error);
+	}
+}
+
+function GetCloudBuildProjectName(user)
+{
+	var projectsMap = {};
+	projectsMap['U4KSJ2RJ8'] = "prototypes-oles"; //terko
+	projectsMap['U58MWTQ1J'] = "prototypes-ostap"; //@oz
+	projectsMap['U585UFVM3'] = "prototypes-nazar"; //@naz0r
+	projectsMap['U7N52PR6X'] = "prototypes-sanya"; //@Oleksandr Orchakov 
+	projectsMap['U7LJKNF96'] = "prototypes-max"; //@Max Karpinsky 
+	projectsMap['UC94FSJ4V'] = "prototypes-adriyan"; //@purplebread 
+	projectsMap['U59UF4SF8'] = "prototypes-borys"; //@bromm
+	projectsMap['UC1N5KQ4F'] = "prototypes-romko"; //@romko
+	projectsMap['U6BNEMWKD'] = "prototypes-maryan"; //@Maryan Tytyunnuk
+	projectsMap['U9Z1M4TKK'] = "prototypes-sasha"; //@Alexandr Doroshenko
+	
+	return projectsMap[user];
+}
+
+function GetCloudBuildTargetURL(user, platform)
+{
+	return  'https://build-api.cloud.unity3d.com/api/v1/orgs/terko/projects/' + GetCloudBuildProjectName(user) + "/buildtargets/" + platform;
+}
+
+function GetStartBuildURL(user, platform)
+{
+	return 'https://build-api.cloud.unity3d.com/api/v1/orgs/terko/projects/' + GetCloudBuildProjectName(user) + "/buildtargets/" + platform + "/builds";
+}
+
+async function UpdateBuildTarget(user, branch, platform)
+{
+	var body = {};
+	body.settings = {};
+	body.settings.scm = {};
+	body.settings.scm.branch = branch;
+
+	headers = { "Content-type" : "application/json",
+			'Authorization': 'Basic b182563c74cc832f104816a9ecdeee15'};
+
+	await fetch(GetCloudBuildTargetURL(user, platform), { method: 'PUT', headers: headers, body:  JSON.stringify(body)});
+console.log(GetStartBuildURL(user, platform));
+	await fetch(GetStartBuildURL(user, platform), { method: 'POST', headers: headers});
+	//const buildTargetResponseJson = await buildTargetResponse.json();	
+}
+
+var startBuildURL = 'https://build-api.cloud.unity3d.com/api/v1/orgs/terko/projects/prototypes-oles/buildtargets/_all/builds';
+
+async function onBranchSelected(bot, message)
+{
+	var error = null;
+	var value =  JSON.parse(message.actions[0].selected_option.value);
+
+	var user = value.user;
+	var branch = value.branch;
+
+	const result = await SelectPlatform(bot, message, user, branch);
+
+	// await bot.replyEphemeral(message, result);
+	
+	if (error)
+	{
+		await bot.replyInteractive(message, error);
+	}
+}
+
+function GetPlatformSelectionBlocks(user, branch)
+{
+	var iosValue = {};
+	iosValue.user = user;
+	iosValue.branch = branch;
+	iosValue.platform = "ios";
+	
+	var androidValue = {};
+	androidValue.user = user;
+	androidValue.branch = branch;
+	androidValue.platform = "android";
+	
+	return {blocks: [
+	{
+		"type": "actions",
+		"elements": [
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"emoji": true,
+					"text": "iOS"
+				},
+				"style": "primary",
+				"value": JSON.stringify(iosValue)
+			},
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"emoji": true,
+					"text": "Android"
+				},
+				"style": "primary",
+				"value": JSON.stringify(androidValue)
+			},
+			{
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"emoji": true,
+					"text": "Cancel"
+				},
+				"style": "danger",
+				"value": "cancel"
+			}
+		]
+	}
+]};
+}
+
+async function SelectPlatform(bot, message, user, branch)
+{
+	await bot.replyInteractive(message, GetPlatformSelectionBlocks(user, branch));
+}
+
+async function onButtonSelected(bot, message)
+{
+	if (message.actions[0].value == "cancel")
+	{
+		bot.replyInteractive(message, "Build canceled");
+		// bot.deleteMessage(message);
+	}
+	else
+	{
+		var value = JSON.parse(message.actions[0].value);
+
+		var user = value.user;
+		var branch = value.branch;
+		var platform = value.platform;
+
+		const result = await UpdateBuildTarget(user, branch, platform);
+		await bot.replyInteractive(message, "*" + platform + " " + branch + "* build successfuly initiated! :check:");
+	}
+}
+
+module.exports = function(controller) {
+
+    controller.hears('sample','message,direct_message', async(bot, message) => {
+        await bot.reply(message, 'I heard a sample message.');
+    });
+
+    controller.on('message,direct_message', async(bot, message) => 
+    {
+		if (message.actions.length > 0)
+		{
+			console.log(message.actions[0]);
+			if (message.actions[0].type == 'users_select') 
+			{
+				await onUserSelected(bot, message);
+			}
+			else if (message.actions[0].type == 'static_select')
+			{
+				await onBranchSelected(bot, message);
+			}
+			else if (message.actions[0].type == "button")
+			{
+				await onButtonSelected(bot, message);
+			}
+		}
+    });
+
+}
+
+
---
+++ features/slack_features.js
@@ -0,0 +1,226 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+const { SlackDialog } = require('botbuilder-adapter-slack');
+
+module.exports = function(controller) {
+
+    controller.ready(async () => {
+        if (process.env.MYTEAM) {
+            let bot = await controller.spawn(process.env.MYTEAM);
+            await bot.startConversationInChannel(process.env.MYCHAN,process.env.MYUSER);
+            bot.say('I AM AWOKEN.');
+        }
+    });
+
+    controller.on('direct_message', async(bot, message) => {
+        await bot.reply(message,'I heard a private message');
+    });
+
+    controller.hears('dm me', 'message', async(bot, message) => {
+        await bot.startPrivateConversation(message.user);
+        await bot.say(`Let's talk in private.`);
+    });
+
+    controller.on('direct_mention', async(bot, message) => {
+        await bot.reply(message, `I heard a direct mention that said "${ message.text }"`);
+    });
+
+    controller.on('mention', async(bot, message) => {
+        await bot.reply(message, `You mentioned me when you said "${ message.text }"`);
+    });
+
+    controller.hears('ephemeral', 'message,direct_message', async(bot, message) => {
+        await bot.replyEphemeral(message,'This is an ephemeral reply sent using bot.replyEphemeral()!');
+    });
+
+    controller.hears('threaded', 'message,direct_message', async(bot, message) => {
+        await bot.replyInThread(message,'This is a reply in a thread!');
+
+        await bot.startConversationInThread(message.channel, message.user, message.incoming_message.channelData.ts);
+        await bot.say('And this should also be in that thread!');
+    });
+
+    controller.hears('blocks', 'message', async(bot, message) => {
+
+        await bot.reply(message,{
+            blocks: [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Hello, I am here to help you build stuff! *Please select a user:*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "users_select",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": "Select a user",
+                                "emoji": true
+                            }
+                        }
+                    ]
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Button",
+                                "emoji": true
+                            },
+                            "value": "click_me_123"
+                        }
+                    ]
+                }
+            ]
+        }  
+    );
+});
+    controller.on('block_actions', async (bot, message) => {
+        console.log('block_actions', message);
+        await bot.reply(message, `Sounds like your choice is ${ message.incoming_message.channelData.actions[0].value }`)
+    });
+
+async function selectUser(bot, message)
+{
+     // await bot.replyWithDialog(message, new SlackDialog('this is a dialog', '123', 'Submit', [
+     //            {
+     //          "label": "User",
+     //          "name": "user",
+     //          "type": "select",
+     //          "data_source": "users"
+     //        },
+     //        {
+     //          "label": "Branch",
+     //          "type": "select",
+     //          "name": "branches",
+     //          "options": [
+     //            {
+     //              "label": "Hindu (Indian) vegetarian",
+     //              "value": "hindu"
+     //            },
+     //            {
+     //              "label": "Strict vegan",
+     //              "value": "vegan"
+     //            },
+     //            {
+     //              "label": "Kosher",
+     //              "value": "kosher"
+     //            },
+     //            {
+     //              "label": "Just put it in a burrito",
+     //              "value": "burrito"
+     //            } 
+     //      ]
+     //    },
+     //    {
+     //      "label": "Bug ticket",
+     //      "name": "ticket_list",
+     //      "type": "select",
+     //      "data_source": "external"
+     //    }
+     //            ]).notifyOnCancel(true).state('foo').asObject());
+
+    await bot.replyInteractive(message,{
+            blocks: [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Hello, I am here to help you build stuff! \n*Please select a user:*"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "users_select",
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": "Select a user",
+                                "emoji": true
+                            }
+                        }
+                    ]
+                }
+            ]
+        }  
+    );
+}
+    controller.on('slash_command', async(bot, message) => {
+        if (message.text === 'plain') {
+            await bot.reply(message, 'This is a plain reply');
+        } else if (message.text === 'public') {
+            await bot.replyPublic(message, 'This is a public reply');
+        } else if (message.text === 'private') {
+            await bot.replyPrivate(message, 'This is a private reply');
+        }
+        await selectUser(bot, message);
+        // set http status
+        //bot.httpBody({text:'You can send an immediate response using bot.httpBody()'});
+
+    });
+
+    controller.on('interactive_message', async (bot, message) => {
+
+        console.log('INTERACTIVE MESSAGE', message);
+
+        switch(message.actions[0].name) {
+            case 'replace':
+                await bot.replyInteractive(message,'[ A previous message was successfully replaced with this less exciting one. ]');
+                break;
+            case 'dialog':
+                await bot.replyWithDialog(message, new SlackDialog('this is a dialog', '123', 'Submit', [
+                    {
+                        type: 'text',
+                        label: 'Field 1',
+                        name: 'field1',
+                    },
+                    {
+                        type: 'select',
+                        label: 'Selection',
+                        name: 'field2',
+                        options: ['iOS', 'Android']
+                    }
+                ]).notifyOnCancel(true).state('foo').asObject());
+                break;
+            default:
+                await bot.reply(message, 'Got a button click!');
+        }
+    });
+
+
+    controller.on('dialog_submission', async (bot, message) => {
+        await bot.reply(message, 'Got a dialog submission');
+
+        // Return an error to Slack
+        bot.dialogError([
+            {
+                "name": "field1",
+                "error": "there was an error in field1"
+            }
+        ])
+    });
+
+    controller.on('dialog_cancellation', async (bot, message) => {
+        await bot.reply(message, 'Got a dialog cancellation');
+    });
+
+}
