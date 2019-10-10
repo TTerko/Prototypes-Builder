76ce49d08a809796c722dd078d614a58cb4f4a24
Oles Terletskyy <oles.terletskiy@gmail.com>
2019-10-10 03:15:00 +0300

    Add mongodb

--- bot.js
+++ bot.js
@@ -3,8 +3,43 @@ var express = require('express');
 const { App, ExpressReceiver } = require('@slack/bolt');
 var bodyParser = require('body-parser');
 const fetch = require("node-fetch");
-var db = require('./features/db-integration');
+
 var slackIntegration = require("./features/slack-integration");
+
+const mongo = require('mongodb');
+
+const url = "mongodb+srv://terko:terkoterko@cluster0-vwvvj.mongodb.net/test?retryWrites=true&w=majority";
+
+var dbo = {};
+var runningBuildsCollection = {};
+
+mongo.connect(url, {useNewUrlParser: true}, (err, database) => {
+        if(err) {
+           console.log(err);
+           process.exit(0);
+        }
+
+        dbo = database.db('slack-integration');
+
+        runningBuildsCollection = dbo.collection('running_builds');
+        console.log("database connected");
+}); 
+
+async function updateRunner(value, user)
+{
+    await runningBuildsCollection.updateOne({build: value}, {'$set': {'user': user}}, {"upsert" : true}, (err, results) => {
+        
+    });
+}
+async function getRunner (value)
+{
+    return await runningBuildsCollection.findOne({build: value});
+}
+
+var db = {
+    updateRunner : updateRunner,
+    getRunner : getRunner
+};
 
 console.log("start");
 
